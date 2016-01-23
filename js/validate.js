730d9ee3256041f6890f854fdb479caf055332c4
AlexanderDegterev <Alexander.1@mail.ru>
2016-01-23 22:48:10 +0300

    js validate

--- js/validate.js
+++ js/validate.js
@@ -20,27 +20,73 @@ function showError(container, errorMessage) {
 
 // добавление в конец
     list.appendChild(li)
-
-
 }
+
+function showError2(numberError, errorMessage) {
+    var classForError = document.getElementsByClassName('error')[numberError];
+    classForError.innerHTML = errorMessage;
+}
+
+function resetError2(numberError) {
+    var classForError = document.getElementsByClassName('error')[numberError];
+    classForError.innerHTML = '';
+}
+
+
 
 function resetError(container) {
 //    container.className = '';
-    if (container.lastChild.className == "error-message") {
+    if (container.lastChild.className == "error") {
         container.removeChild(container.lastChild);
     }
 }
 
 function validate(form) {
+
     var elems = form.elements;
 
-    resetError(elems.name.parentNode);
+//    resetError(elems.name.parentNode);
+
+//    var url2 = elems.name.nextSibling;
+//    alert(url2);
+//    var a = elems.name.nextSibling;
+//    alert(a);
+//    var b = a.nextSibling;
+//    alert(b);
+//    var c = a.getElementsByTagName('p').innerHTML;
+//    alert(c);
+//    var tag2 = document.getElementsByTagName('p')[0].innerHTML;
+//    alert(tag2);
+    var numberError = 0;
+    resetError2(numberError);
     if (!elems.name.value) {
-        showError(elems.name.parentNode, ' Укажите от кого.');
-        console.log(elems.name.parentNode+'parentNode121');
+
+        var errorMessage='Enter your name';
+        showError2(numberError,errorMessage);
+//        var div = document.getElementsByClassName('error')[0];
+//        SummSumm='Enter your name';
+//        div.innerHTML = SummSumm;
+//        showError(elems.name.parentNode, ' Укажите от кого.');
+//        console.log(elems.name.parentNode+'parentNode121');
 
     }
 
+    numberError = 1;
+    resetError2(numberError);
+//    resetError(elems.email.parentNode);
+    if (!elems.email.value) {
+        errorMessage = 'Enter your e-mail';
+        showError2(numberError, errorMessage);
+    } else {
+        var regex = /^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]([a-zA-Z]{2,9})$/;
+        var email = elems.email.value;
+        if (!regex.test(email)) {
+            errorMessage = 'Enter a valid email';
+            showError2(numberError, errorMessage);
+        }
+    }
+
+    // переделать пароль
     resetError(elems.password.parentNode);
     if (!elems.password.value) {
         showError(elems.password.parentNode, ' Укажите пароль.');
@@ -48,14 +94,19 @@ function validate(form) {
         showError(elems.password.parentNode, ' Пароли не совпадают.');
     }
 
-//    resetError(elems.to.parentNode);
-//    if (!elems.to.value) {
-//        showError(elems.to.parentNode, ' Укажите, куда.');
-//    }
 
-    resetError(elems.msg.parentNode);
+//    resetError(elems.msg.parentNode);
+
+    var numberError = 4;
+    alert(elems.msg.value);
+//    resetError2(numberError);
     if (!elems.msg.value) {
-        showError(elems.msg.parentNode, ' Отсутствует текст.');
+
+        var errorMessage='Enter your test';
+        showError2(numberError,errorMessage);
     }
+//    if (!elems.msg.value) {
+//        showError(elems.msg.parentNode, ' Отсутствует текст.');
+//    }
 
 }
