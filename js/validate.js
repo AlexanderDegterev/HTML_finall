7b6a3c77b148e38a9533d69088f7c4e0745e047e
AlexanderDegterev <Alexander.1@mail.ru>
2016-01-25 23:22:46 +0300

    no message

--- js/validate.js
+++ js/validate.js
@@ -18,13 +18,17 @@ function showError2(numberError, errorMessage) {
     classForError.innerHTML = errorMessage;
 }
 
-function showError3(errorMessage) {
-    var classForError = document.getElementById('name-error');
+function showError3(idError,errorMessage) {
+    var classForError = document.getElementById(idError);
     classForError.innerHTML = errorMessage;
 }
 
-function resetError2(numberError) {
-    var classForError = document.getElementsByClassName('error')[numberError];
+//function resetError2(numberError) {
+//    var classForError = document.getElementsByClassName('error')[numberError];
+//    classForError.innerHTML = '';
+//}
+function resetError2(idError) {
+    var classForError = document.getElementById(idError);
     classForError.innerHTML = '';
 }
 
@@ -37,23 +41,24 @@ function resetError2(numberError) {
 
 function validate(form) {
     var elems = form.elements;
-//    var a = elems.from.parentNode;
-    alert(elems);
 
-    var numberError = 0;
-    resetError2(numberError);
+//    var numberError = 0;
+    var idError = "name-error";
+    alert(idError);
+    resetError2(idError);
     if (!elems.name.value) {
 
         var errorMessage = 'Enter your name';
-        showError3(errorMessage);
+        showError3(idError,errorMessage);
 //        showError2(numberError, errorMessage);
     }
 
-    numberError = 3;
-    resetError2(numberError);
+//    numberError = 3;
+    var idError = "email-error";
+    resetError2(idError);
     if (!elems.email.value) {
         errorMessage = 'Enter your e-mail';
-        showError3(errorMessage);
+        showError3(idError,errorMessage);
 //        showError2(numberError, errorMessage);
     } else {
         var regex = /^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]([a-zA-Z]{2,9})$/;
@@ -61,33 +66,39 @@ function validate(form) {
         if (!regex.test(email)) {
             errorMessage = 'Enter a valid email';
 //            showError2(numberError, errorMessage);
-            showError3(errorMessage);
+            showError3(idError,errorMessage);
         }
     }
 
-    numberError = 1;
-    resetError2(numberError);
+//    numberError = 1;
+    var idError = "pass-error";
+    resetError2(idError);
     if (!elems.password.value) {
         errorMessage = 'Enter your password';
-        showError2(numberError, errorMessage);
+//        showError2(numberError, errorMessage);
+        showError3(idError,errorMessage);
     } else {
         var regexPass = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
         var pass = elems.password.value;
         if (!regexPass.test(pass)) {
             errorMessage = 'at least one number, one lowercase and one uppercase letter, at least six characters';
-            showError2(numberError, errorMessage);
+//            showError2(numberError, errorMessage);
+            showError3(idError,errorMessage);
         }
         if (elems.password.value != elems.password2.value) {
             errorMessage = 'Passwords do not match !';
-            showError2(numberError, errorMessage);
+//            showError2(numberError, errorMessage);
+            showError3(idError,errorMessage);
         }
     }
 
-    var numberError = 5;
-    resetError2(numberError);
+//    var numberError = 5;
+    var idError = "msg-error";
+    resetError2(idError);
     if (!elems.msg.value) {
 
         var errorMessage = 'Enter your text';
-        showError2(numberError, errorMessage);
+//        showError2(numberError, errorMessage);
+        showError3(idError,errorMessage);
     }
 }
